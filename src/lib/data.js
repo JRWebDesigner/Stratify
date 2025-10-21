fb1e095e626cf8b6d569ca77ec593a076664486e
Mamani Agreda Josue Martin <jowebdesigc@gmail.com>
2025-10-21 17:18:31 -0400

    factorizado

---
+++ src/lib/data.js
@@ -0,0 +1,43 @@
+export const menuPrincipal=[
+  {
+    name:'Inicio',
+    href:'/'
+  },
+  {
+    name:'Cotizar',
+    href:'/cotizar'
+  },
+  {
+    name:'Nuestros Servicios',
+    href:'/servicios'
+  },
+  {
+    name:'Proyectos',
+    href:'/proyectos'
+  },
+  {
+    name:'Contactos',
+    href:'/contactos'
+  },
+]
+
+export const proyectos=[
+	{
+		image: '/fondo.jpg',
+		site:'https://techsolutions.demo',
+		title: 'TechSolutions',
+		description: 'Desarrollo de una plataforma de gestión empresarial para pequeñas y medianas empresas.',
+		technologies: ['React', 'Node.js', 'MongoDB'],
+		time:'2 semanas',
+		type:'corporativo'
+	},
+  {
+		image: '/fondo.jpg',
+		site:'https://techsolutions.demo',
+		title: 'TechSolutions',
+		description: 'Desarrollo de una plataforma de gestión empresarial para pequeñas y medianas empresas.',
+		technologies: ['React', 'Node.js', 'MongoDB'],
+		time:'2 semanas',
+		type:'corporativo'
+	}
+]
